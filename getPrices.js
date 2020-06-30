7534b1e9024a62d537ac6bfb11ec465673bac5ea
James McConnon <jamesmcconnon2@gmail.com>
2020-06-30 22:41:56 +0100

    update to run on schedule uses node scheduler to run it on a schedule then saves a copy to the bot folder.

--- getPrices.js
+++ getPrices.js
@@ -4,30 +4,37 @@ const { bitskins_key, bitskins_secret } = require('./config.json');
 const totp = require('notp').totp;
 const base32 = require('thirty-two');
 
+const schedule = require('node-schedule');
 
 
-// Set up the secret.
-const secret = totp.gen(base32.decode(bitskins_secret));
+const j = schedule.scheduleJob('30 * * * * *', function(){
+    console.log('Running bitskins prices on schedule.');
+    // Set up the secret.
+    const secret = totp.gen(base32.decode(bitskins_secret));
 
-// Set up axios call
-const baseUrl = 'https://bitskins.com/api/v1';
-const endpoint = `/get_all_item_prices/?api_key=${bitskins_key}&app_id=730&code=${secret}`
-const url = `${baseUrl}${endpoint}`;
+    // Set up axios call
+    const baseUrl = 'https://bitskins.com/api/v1';
+    const endpoint = `/get_all_item_prices/?api_key=${bitskins_key}&app_id=730&code=${secret}`
+    const url = `${baseUrl}${endpoint}`;
 
-// Start a timer to check response time from API
-console.time('apiResponseTime');
-// Get prices from bitskins
-axios.get(url)
-    .then(function(response) {
-      console.log('Got the bitskins prices');
-      const receivedItems = response.data;
-      const saveableItems = JSON.stringify(receivedItems);
-      fs.writeFile('prices.json', saveableItems, 'utf8', () => {
-        console.log('Written to json file');
-      });
-    })
-    .catch(err => {
-      console.error(err.message);
-    });
+    // Start a timer to check response time from API
+    console.time('apiResponseTime');
+    // Get prices from bitskins
+    axios.get(url)
+        .then(function(response) {
+          console.log('Got the bitskins prices');
+          const receivedItems = response.data;
+          const saveableItems = JSON.stringify(receivedItems);
+          fs.writeFile('prices.json', saveableItems, 'utf8', () => {
+            console.log('Written to local json file');
+          });
+          fs.writeFile('../bot/prices.json', saveableItems, 'utf8', () => {
+            console.log('Written to bots json file');
+          });
+        })
+        .catch(err => {
+          console.error(err.message);
+        });
 
-console.timeEnd('apiResponseTime');
+    console.timeEnd('apiResponseTime');
+});
